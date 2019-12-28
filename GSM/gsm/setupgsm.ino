a4741d8f65e053e8a98e0c00f2f8e9dc4023e2c7
Rioverde <abbasbabayev.m98@gmail.com>
2019-12-28 21:32:44 +0200

    GSM updated

--- GSM/gsm/setupgsm.ino
+++ GSM/gsm/setupgsm.ino
@@ -3,11 +3,17 @@
 
 // PIN Number for the SIM
 #define PINNUMBER ""
+#define LedPin 9
+#define trigPin 4
+#define echoPin 5
+#define LittleLed 11
+
 
 // initialize the library instances
 GSM gsmAccess;
 GSM_SMS sms;
 Servo ServMotor;
+int flag = 0;
 
 // Array to hold the number a SMS is retreived from
 char senderNumber[20];
@@ -15,7 +21,13 @@ int angle = 0;
 
 void setup() {
 
-  ServMotor.attach(10);   
+  
+  ServMotor.attach(10);  
+  pinMode(LedPin, OUTPUT);
+  pinMode(LittleLed, OUTPUT);
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT); 
+  digitalWrite(9,HIGH);
   
   // initialize serial communications and wait for port to open:
   Serial.begin(9600);
@@ -43,12 +55,46 @@ void setup() {
   Serial.println("-----Send Message------");
 }
 
+long duration;
+int distance;
+
 void loop() {
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(5);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  duration = pulseIn(echoPin, HIGH);
+  distance= (duration/2)*0.034;
   char c;
   String MessageIn = "";
+  char remotenumber[] = "+905428876593";
+  char txtMsg[] = "Im Here";
 
-  // If there are any SMSs available()
+    if(distance <= 10)
+    {
+      Serial.println();
+      Serial.println("----Initialized mode as Sending Message-----");
+      if(flag == 0){
+      Serial.println("Sending Message to: ");
+      Serial.print(remotenumber);
+      Serial.println("Message Content: ");
+      Serial.print(txtMsg);
+  
+      sms.beginSMS(remotenumber);
+      sms.print(txtMsg);
+      sms.endSMS();
+      Serial.println("\nMessage Sent Successifully\n");
+      flag++;
+      }
+      flag = 0;
+      delay(5000);
+      
+    }
+
+  // If there are any SMSs available
   if (sms.available()) {
+
     Serial.println("Message received from:");
 
     // Get remote number
@@ -79,11 +125,11 @@ void loop() {
 
     if(MessageIn == "LedOn")
     {
-        digitalWrite(9,HIGH);
+        digitalWrite(9,LOW);
         delay(50);
     }
       else if(MessageIn == "LedOff"){
-        digitalWrite(9,LOW);
+        digitalWrite(9,HIGH);
         delay(50);
 
       }
@@ -104,6 +150,12 @@ void loop() {
             } 
 
       }
+
+       else if(MessageIn == "Led3"){
+        digitalWrite(3,HIGH);
+        delay(50);
+
+      }
       
       
 
@@ -114,8 +166,10 @@ void loop() {
     
     Serial.println("------------------");
     Serial.println("TASKS DONE");
+    delay(500);
+
   }
 
-  delay(1000);
+  delay(2000);
 
 }
